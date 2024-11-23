7f968b6357206588d3d3259c6301b01b07b6a271
Mike Williams <mike@buildlab.co>
2024-11-22 23:00:56 -0500

    cleaned up

--- src/routes/api/[...paths]/+server.ts
+++ src/routes/api/[...paths]/+server.ts
@@ -1,3 +1,20 @@
-import { api } from '$lib/api';
+import type { RequestHandler } from '@sveltejs/kit';
+import { Hono } from 'hono';
 
-export const GET = ({ request }) => api.fetch(request);
+const app = new Hono().basePath('/api');
+
+app.get('/hello', (c) => {
+	return c.json({
+		message: 'Hello from Hono on Vercel!'
+	});
+});
+
+app.get('/:wild', (c) => {
+	const wild = c.req.param('wild');
+	return c.json({
+		message: `Hello from Hono on Vercel! You're now on /api/${wild}!`
+	});
+});
+
+export const GET: RequestHandler = ({ request }) => app.fetch(request);
+export type Router = typeof app;
